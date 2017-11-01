c6bf00c66f7d443f945ac0eb0bfc6451b0e78311
Adam <adam.p.bannister@gmail.com>
2017-11-01 22:49:56 +0100

    add websocket send on connection to register connection, save hostname and websocket in dict for active connections

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from socket import gethostname
 from operator import itemgetter
 from sanic import Sanic
 from sanic import response
+from websockets import ConnectionClosed
 
 
 app = Sanic(__name__)
@@ -24,8 +25,7 @@ restaurant_dict = {
     'Gruene': 0,
     'Audi': 0,
 }
-
-
+clients = {}
 voters = {r: set() for r in restaurant_dict}
 
 
@@ -50,16 +50,25 @@ async def restaurants(request):
 @app.websocket('/update/')
 async def update(request, ws):
     while True:
-        restaurant = await ws.recv()
-        hostname = gethostname()
-        this_vote = voters.get(restaurant)
-        if not this_vote:
-            if hostname not in this_vote:
-                voters[restaurant].add(hostname)
-                restaurant_dict[restaurant] += 1
-                await ws.send(restaurant)
+        data = json.loads(await ws.recv())
+        restaurant = None
+        if data['message'] == 'online':
+            hostname = gethostname()
+            clients[hostname] = ws
         else:
-            await ws.send('406')
+            restaurant = data['message']
+            this_vote = voters.get(restaurant)
+            if restaurant and not this_vote:
+                if hostname not in this_vote:
+                    voters[restaurant].add(hostname)
+                    restaurant_dict[restaurant] += 1
+                    for client in clients.values():
+                        try:
+                            await client.send(restaurant)
+                        except ConnectionClosed:
+                            del clients[client]
+            else:
+                await ws.send('406')
 
 
 if __name__ == '__main__':
