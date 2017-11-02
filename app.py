1e8ec44c8662c344b59da2b87b30ff3d920305cc
Adam <adam.p.bannister@gmail.com>
2017-11-02 13:06:04 +0100

    move vote handling to its own function

--- app.py
+++ app.py
@@ -52,23 +52,27 @@ async def update(request, ws):
     while True:
         data = json.loads(await ws.recv())
         restaurant = None
+        hostname = gethostname()
         if data['message'] == 'online':
-            hostname = gethostname()
             clients[hostname] = ws
         else:
             restaurant = data['message']
-            this_vote = voters.get(restaurant)
-            if restaurant and not this_vote:
-                if hostname not in this_vote:
-                    voters[restaurant].add(hostname)
-                    restaurant_dict[restaurant] += 1
-                    for client in clients.values():
-                        try:
-                            await client.send(restaurant)
-                        except ConnectionClosed:
-                            del clients[client]
-            else:
-                await ws.send('406')
+            handle_vote(ws, restaurant, hostname)
+
+
+async def handle_vote(ws, restaurant, hostname):
+    this_vote = voters.get(restaurant)
+    if restaurant and not this_vote:
+        if hostname not in this_vote:
+            voters[restaurant].add(hostname)
+            restaurant_dict[restaurant] += 1
+            for client in clients.values():
+                try:
+                    await client.send(restaurant)
+                except ConnectionClosed:
+                    del clients[client]
+    else:
+        await ws.send('406')
 
 
 if __name__ == '__main__':
